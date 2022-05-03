c562dee7f563ba3d4715a67265198f75c0ba5d0e
Surya Teja Palavalsa <mail@suryateja.dev>
2022-05-03 16:35:39 -0700

    login api

--- auth.py
+++ auth.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from flask_login import login_user, login_required, logout_user
 
 auth = Blueprint('auth', __name__)
 
@@ -7,10 +10,46 @@ auth = Blueprint('auth', __name__)
 def login():
     return render_template('login.html')
 
+@auth.route('/login', methods=['POST'])
+def login_post():
+
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        return redirect(url_for('auth.login'))
+    
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile'))
+
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
 
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        return redirect(url_for('auth.signup'))
+
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('auth.login'))
+
+@login_required
 @auth.route('/logout')
 def logout():
-    return 'Logout'
+    logout_user()
+    return 'Logged Out'
